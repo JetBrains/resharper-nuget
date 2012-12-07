e498b4c93692d95dbc8ca82c9615dad77a5a9d23
Matt Ellis <matt.ellis@jetbrains.com>
2012-12-07 16:48:31 +0000

    Bumped build number

--- src/resharper-nuget/Properties/AssemblyInfo.cs
+++ src/resharper-nuget/Properties/AssemblyInfo.cs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using System.Reflection;
 using JetBrains.Application.PluginSupport;
 
@@ -13,8 +29,7 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
 [assembly: PluginTitle("NuGet support for ReSharper")]
