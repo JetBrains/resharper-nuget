712d2569cc73b2fea56e9d99b7cca15aa0288806
Matt Ellis <matt.ellis@jetbrains.com>
2013-01-18 17:20:57 +0000

    Bumped version to 1.1

--- src/resharper-nuget/Properties/AssemblyInfo.cs
+++ src/resharper-nuget/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.1.0.*")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
 [assembly: PluginTitle("NuGet support for ReSharper")]
