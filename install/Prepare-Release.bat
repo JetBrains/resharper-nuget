e77cf319941047ba0a3e4f28bf18499e42916bd0
Matt Ellis <matt.ellis@jetbrains.com>
2013-01-24 10:11:40 +0000

    Removed ReSharper 7.0 from installer batch files

--- install/Prepare-Release.bat
+++ install/Prepare-Release.bat
@@ -4,8 +4,5 @@ setlocal enableextensions
 mkdir NuGetSupport.6.1 2> NUL
 copy /y ..\src\resharper-nuget\bin\Release\*.6.1.* NuGetSupport.6.1\
 
-mkdir NuGetSupport.7.0 2> NUL
-copy /y ..\src\resharper-nuget\bin\Release\*.7.0.* NuGetSupport.7.0\
-
 mkdir NuGetSupport.7.1 2> NUL
 copy /y ..\src\resharper-nuget\bin\Release\*.7.1.* NuGetSupport.7.1\
