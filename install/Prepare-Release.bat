c46dc882186b2636ae8e53bb4e8d00e43ec6584e
Thomas Freudenberg <info@thomasfreudenberg.com>
2013-04-07 15:15:48 +0200

    added ReSharper 8 to batch files

--- install/Prepare-Release.bat
+++ install/Prepare-Release.bat
@@ -6,3 +6,6 @@ copy /y ..\src\resharper-nuget\bin\Release\*.6.1.* NuGetSupport.6.1\
 
 mkdir NuGetSupport.7.1 2> NUL
 copy /y ..\src\resharper-nuget\bin\Release\*.7.1.* NuGetSupport.7.1\
+
+mkdir NuGetSupport.8.0 2> NUL
+copy /y ..\src\resharper-nuget\bin\Release\*.8.0.* NuGetSupport.8.0\
